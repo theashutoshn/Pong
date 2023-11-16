4524cfb87cf9bb91d988b0453037316c5db888a3
AshutoshNandanwar <ashunandanwar98@gmail.com>
2023-11-16 22:24:23 +0530

    Red & Blue Player Movement

---
+++ Assets/Scenes/Scripts/BluePlayer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePlayer : MonoBehaviour
+{
+    private float _speed = 10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var velocity = rb.velocity;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity.y = _speed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity.y = -_speed;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+
+        rb.velocity = velocity;
+
+        var position = transform.position;
+        if (position.y > 4.07f)
+        {
+            position.y = 4.07f;
+        }
+        else if (position.y < -4.07f)
+        {
+            position.y = -4.07f;
+        }
+
+        transform.position = position;
+    }
+}
